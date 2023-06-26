c009e9143958bb9c2fe072417ea119994539d1c7
Lee In Seok <dlstjr9068@gmail.com>
2023-06-26 22:55:03 +0900

    1. JDBC Lock 추가 2. AsyncService 로깅 추가

--- src/main/java/kr/co/sample/configs/LockConfig.java
+++ src/main/java/kr/co/sample/configs/LockConfig.java
@@ -3,7 +3,12 @@ package kr.co.sample.configs;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.integration.jdbc.lock.DefaultLockRepository;
+import org.springframework.integration.jdbc.lock.JdbcLockRegistry;
+import org.springframework.integration.jdbc.lock.LockRepository;
 import org.springframework.integration.redis.util.RedisLockRegistry;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class LockConfig {
@@ -13,5 +18,15 @@ public class LockConfig {
     @Bean
     RedisLockRegistry redisLockRegistry(LettuceConnectionFactory lettuceConnectionFactory) {
         return new RedisLockRegistry(lettuceConnectionFactory, LOCK_PREFIX);
+    }
+
+    @Bean
+    public LockRepository lockRepository(DataSource datasource) {
+        return new DefaultLockRepository(datasource);
+    }
+
+    @Bean
+    public JdbcLockRegistry jdbcLockRegistry(LockRepository repository) {
+        return new JdbcLockRegistry(repository);
     }
 }
--- src/main/java/kr/co/sample/services/AsyncService.java
+++ src/main/java/kr/co/sample/services/AsyncService.java
@@ -1,16 +1,19 @@
 package kr.co.sample.services;
 
 import kr.co.sample.dtos.common.ResultGenericDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.CompletableFuture;
 
+@Slf4j
 @Service
 public class AsyncService {
 
     @Async
     public CompletableFuture<ResultGenericDto<String>> callAsync() {
+        log.info("callAsync start");
         return CompletableFuture.completedFuture(ResultGenericDto.ofSuccess("Hello"));
     }
 }
