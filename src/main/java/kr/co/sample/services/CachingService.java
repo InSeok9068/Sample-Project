73c85e33802de08824d02194b15c059035171a40
Lee In Seok <dlstjr9068@gmail.com>
2023-06-27 13:19:13 +0900

    1. 캐시 Not Annotation 삭제 작업

--- src/main/java/kr/co/sample/services/CachingService.java
+++ src/main/java/kr/co/sample/services/CachingService.java
@@ -2,19 +2,26 @@ package kr.co.sample.services;
 
 import kr.co.sample.dtos.common.ResultDto;
 import kr.co.sample.dtos.common.ResultGenericDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Service
+@RequiredArgsConstructor
 public class CachingService {
-
     private static final Map<String, String> CACHE_DATA_MAP = new ConcurrentHashMap<>();
+    private static final String CACHE_NAME = "cache";
 
-    @Cacheable(value = "cache", key = "#key")
+    private final CacheManager cacheManager;
+
+    @Cacheable(value = CACHE_NAME, key = "#key")
     public ResultGenericDto<String> getData(String key) {
         try {
             Thread.sleep(1000);
@@ -29,9 +36,16 @@ public class CachingService {
         return ResultDto.ofSuccess();
     }
 
-    @CacheEvict(value = "cache", key = "#key")
+    @CacheEvict(value = CACHE_NAME, key = "#key")
     public ResultDto removeData(String key) {
         CACHE_DATA_MAP.remove(key);
         return ResultDto.ofSuccess();
+    }
+
+    private void removeCaching() {
+        Cache cache = cacheManager.getCache(CACHE_NAME);
+        if (Objects.nonNull(cache)) {
+            cache.clear();
+        }
     }
 }
