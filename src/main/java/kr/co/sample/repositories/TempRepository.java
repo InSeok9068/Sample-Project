806ece02c8dd17f4cdaf05b107ae4b8a2542e3c3
Lee In Seok <dlstjr9068@gmail.com>
2023-07-02 21:05:00 +0900

    업데이트문 작성

--- src/main/java/kr/co/sample/repositories/TempRepository.java
+++ src/main/java/kr/co/sample/repositories/TempRepository.java
@@ -2,14 +2,23 @@ package kr.co.sample.repositories;
 
 import kr.co.sample.entitys.TempEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 public interface TempRepository extends JpaRepository<TempEntity, Long> {
 
     @Query("""
-            select t.id 
-            from TempEntity t 
+            select t.id
+            from TempEntity t
             where t.id = :id
             """)
     TempEntity findByIdForQuery(Long id);
+
+    @Modifying(clearAutomatically = true) // 1차 캐싱을 삭제
+    @Query("""
+            update TempEntity t
+            set t.updateDate = now()
+            where t.id = :id
+            """)
+    void updateQuery(Long id);
 }
