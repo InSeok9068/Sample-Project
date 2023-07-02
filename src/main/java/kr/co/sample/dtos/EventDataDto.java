f20ccef2c50b367b61fec03088910db488cf302b
Lee In Seok <dlstjr9068@gmail.com>
2023-07-02 23:50:49 +0900

    추상화 객체

--- src/main/java/kr/co/sample/dtos/EventDataDto.java
+++ src/main/java/kr/co/sample/dtos/EventDataDto.java
@@ -1,70 +1,70 @@
 package kr.co.sample.dtos;
 
 import kr.co.sample.dtos.common.EventDto;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
-import lombok.experimental.SuperBuilder;
 
 public class EventDataDto {
 
     @Getter
-    @SuperBuilder
+    @Builder
     @ToString(callSuper = true)
     public static class Event extends EventDto {
         private final String data;
     }
 
     @Getter
-    @SuperBuilder
+    @Builder
     @ToString(callSuper = true)
     public static class EventAsync extends EventDto {
         private final String data;
     }
 
     @Getter
-    @SuperBuilder
+    @Builder
     @ToString(callSuper = true)
     public static class EventTransaction extends EventDto {
         private final String data;
     }
 
     @Getter
-    @SuperBuilder
+    @Builder
     @ToString(callSuper = true)
     public static class EventTransactionCase1 extends EventDto {
         private final String data;
     }
 
     @Getter
-    @SuperBuilder
+    @Builder
     @ToString(callSuper = true)
     public static class EventTransactionCase2 extends EventDto {
         private final String data;
     }
 
     @Getter
-    @SuperBuilder
+    @Builder
     @ToString(callSuper = true)
     public static class EventTransactionCase3 extends EventDto {
         private final String data;
     }
 
     @Getter
-    @SuperBuilder
+    @Builder
     @ToString(callSuper = true)
     public static class EventTransactionCase4 extends EventDto {
         private final String data;
     }
 
     @Getter
-    @SuperBuilder
+    @Builder
     @ToString(callSuper = true)
     public static class EventTransactionCase5 extends EventDto {
         private final String data;
     }
 
     @Getter
-    @SuperBuilder
+    @Builder
     @ToString(callSuper = true)
     public static class EventTransactionCase6 extends EventDto {
         private final String data;
--- src/main/java/kr/co/sample/dtos/common/EventDto.java
+++ src/main/java/kr/co/sample/dtos/common/EventDto.java
@@ -1,16 +1,16 @@
 package kr.co.sample.dtos.common;
 
-import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
-import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDateTime;
 
 @Getter
 @ToString
-@SuperBuilder
 public abstract class EventDto {
-    @Builder.Default
-    private final LocalDateTime timestamp = LocalDateTime.now();
+    private final LocalDateTime timestamp;
+
+    public EventDto() {
+        timestamp = LocalDateTime.now();
+    }
 }
