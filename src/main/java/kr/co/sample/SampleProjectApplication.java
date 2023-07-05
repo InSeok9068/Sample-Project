d5cc31ef4603d2457aa7b249e54aee57c212173d
Lee In Seok <dlstjr9068@gmail.com>
2023-07-05 13:48:02 +0900

    시작 시 종료 시 이벤트 설정

--- src/main/java/kr/co/sample/SampleProjectApplication.java
+++ src/main/java/kr/co/sample/SampleProjectApplication.java
@@ -1,9 +1,14 @@
 package kr.co.sample;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -17,7 +22,24 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class SampleProjectApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SampleProjectApplication.class, args);
+        SpringApplication application = new SpringApplication(SampleProjectApplication.class);
+        application.addListeners(new ApplicationPidFileWriter());
+        application.run(args);
     }
 
+    /**
+     * ■ 어플리케이션 시작 시 이벤트 ■ 이인석 2023-07-05
+     */
+    @Bean
+    public ApplicationRunner applicationRunner() {
+        return args -> System.out.println("Hello World");
+    }
+
+    /**
+     * ■ 어플리케이션 종료 시 이벤트 ■ 이인석 2023-07-05
+     */
+    @Bean
+    public ApplicationListener<ContextClosedEvent> contextClosedEventApplicationListener() {
+        return args -> System.out.println("Bye World");
+    }
 }
