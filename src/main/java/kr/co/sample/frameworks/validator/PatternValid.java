77cc72afd952deae5d773e347e1450f02f5f055b
Lee In Seok <dlstjr9068@gmail.com>
2023-07-06 15:38:18 +0900

    패턴 검증 추가

--- src/main/java/kr/co/sample/frameworks/validator/PatternValid.java
+++ src/main/java/kr/co/sample/frameworks/validator/PatternValid.java
@@ -20,13 +20,20 @@ public @interface PatternValid {
 
     String DEFAULT_MESSAGE = "유효성 검증에 실패하였습니다.";
     String NUMBER_MESSAGE = "숫자만 입력가능합니다.";
+    String EMAIL_MESSAGE = "올바른 이메일 주소 형식이 아닙니다.";
+    String PHONE_NUMBER_MESSAGE = "올바른 전화번호 형식이 아닙니다.";
+    String ENGLISH_ONLY_MESSAGE = "영어 문자만 입력 가능합니다.";
+    String KOREAN_ONLY_MESSAGE = "한글 문자만 입력 가능합니다.";
 
     @Getter
     @AllArgsConstructor
     enum PATTERN_TYPE {
-
-        DEFAULT("", "Default", ""),
-        NUMBER("^[0-9]*$", "숫자만 허용", NUMBER_MESSAGE);
+        DEFAULT("", "Default", DEFAULT_MESSAGE),
+        NUMBER("^[0-9]*$", "숫자만 허용", NUMBER_MESSAGE),
+        EMAIL("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$", "이메일 주소", EMAIL_MESSAGE),
+        PHONE_NUMBER("^\\d{3}-\\d{3,4}-\\d{4}$", "전화번호", PHONE_NUMBER_MESSAGE),
+        ENGLISH_ONLY("^[A-Za-z]+$", "영어 문자만 허용", ENGLISH_ONLY_MESSAGE),
+        KOREAN_ONLY("^[가-힣]+$", "한글만 허용", KOREAN_ONLY_MESSAGE);
 
         private final String pattern;
         private final String description;
